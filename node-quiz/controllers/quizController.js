31f52e18a6d972ba5864da9d79ab2810a43cd872
Matsuda1129 <mk1529ayukat315takoyaki@yahoo.co.jp>
2020-12-19 19:26:16 +0900

    修正５

--- node-quiz/controllers/quizController.js
+++ node-quiz/controllers/quizController.js
@@ -2,16 +2,9 @@ const express = require('express');
 const Quiz = require('../models/Quiz');
 
 module.exports = {
-  doGetQuiz: 
-  (req, res, next) => {
-    const quizes = Quiz.getQuiz
-      if (!err) {
-       res.status(200).send(quizes);
-       console.log(quizes)
-     } else {
-       res.status(404).send({ err: 'Not Found!' });
-     }
-  }
-}
-    const quizes = Quiz.getQuiz()
-console.log(quizes)
+  doGetQuiz: (req, res, next) => {
+    Quiz.getQuiz().then((quizes) => {
+        res.status(200).send(quizes);
+    });
+  },
+};
